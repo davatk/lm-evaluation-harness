6c7268274d43fa9540811e172839f18d3f4c19a9
David Atkinson <davatk@gmail.com>
2023-06-22 11:54:24 -0700

    QoL

--- evaluate.py
+++ evaluate.py
@@ -1,45 +1,48 @@
 import subprocess
 import json
 import os
+from datetime import datetime
 
 RESULTS_DIR = "output"
 
 
 MODELS = [
-    "huggyllama/llama-30b",
-    "huggyllama/llama-13b",
-    "huggyllama/llama-7b",
-    "decapoda-research/llama-7b-hf",
-    "decapoda-research/llama-13b-hf",
-    "decapoda-research/llama-30b-hf",
-    "decapoda-research/llama-65b-hf",
-    "EleutherAI/pythia-70m",
-    "EleutherAI/pythia-160m",
-    "EleutherAI/pythia-410m",
-    "EleutherAI/pythia-1b",
-    "EleutherAI/pythia-1.4b",
-    "EleutherAI/pythia-2.8b",
-    "EleutherAI/pythia-6.9b",
-    "EleutherAI/pythia-12b",
-    "EleutherAI/gpt-neo-1.3B",
-    "EleutherAI/gpt-neo-2.7B",
-    "EleutherAI/gpt-neo-125m",
-    "EleutherAI/gpt-neox-20b",
     "EleutherAI/gpt-j-6b",
-    "bigscience/bloom-560m",
+    "EleutherAI/gpt-neo-1.3B",
+    "EleutherAI/gpt-neo-125m",
+    "EleutherAI/gpt-neo-2.7B",
+    "EleutherAI/gpt-neox-20b",
+    "EleutherAI/pythia-1.4b",
+    "EleutherAI/pythia-12b",
+    "EleutherAI/pythia-160m",
+    "EleutherAI/pythia-1b",
+    "EleutherAI/pythia-2.8b",
+    "EleutherAI/pythia-410m",
+    "EleutherAI/pythia-6.9b",
+    "EleutherAI/pythia-70m",
+    "THUDM/glm-10b",
+    "THUDM/glm-2b",
+    "bigscience/bloom",
     "bigscience/bloom-1b1",
     "bigscience/bloom-1b7",
     "bigscience/bloom-3b",
+    "bigscience/bloom-560m",
     "bigscience/bloom-7b1",
-    "bigscience/bloom",
-    "facebook/opt-66b",
-    "facebook/opt-350m",
-    "facebook/opt-125m",
+    "decapoda-research/llama-13b-hf",
+    "decapoda-research/llama-30b-hf",
+    "decapoda-research/llama-65b-hf",
+    "decapoda-research/llama-7b-hf",
     "facebook/opt-1.3b",
-    "facebook/opt-6.7b",
-    "facebook/opt-2.7b",
+    "facebook/opt-125m",
     "facebook/opt-13b",
+    "facebook/opt-2.7b",
     "facebook/opt-30b",
+    "facebook/opt-350m",
+    "facebook/opt-6.7b",
+    "facebook/opt-66b",
+    "huggyllama/llama-13b",
+    "huggyllama/llama-30b",
+    "huggyllama/llama-7b",
 ]
 
 TASKS = ",".join(['wikitext', 'penn_treebank', 'lambada_standard', 'mnli'])
@@ -49,6 +52,7 @@ if __name__ == '__main__':
     if not os.path.exists(RESULTS_DIR):
         os.makedirs(RESULTS_DIR)
     for model in MODELS:
+        print(f"{datetime.now().isoformat()}: running {model} on {TASKS}")
         model_fname = model.replace('/', '_')
         try:
             subprocess.run(['python3', 'main.py', '--model', 'hf-causal-experimental',
@@ -56,6 +60,7 @@ if __name__ == '__main__':
                             TASKS, '--batch_size', 'auto', '--write_out', '--output_path',
                             f'{RESULTS_DIR}/{model_fname}.json'], check=True, capture_output=True)
         except subprocess.CalledProcessError as e:
+            print(f"{datetime.now().isoformat()}: ERROR {model} on {TASKS}")
             with open(f'{RESULTS_DIR}/errors.log', 'a') as f:
                 f.write(f'=== {model} ===')
                 f.write(str(e.stdout))
